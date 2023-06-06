75618d58e43964494c36047f83f81baf9906a5a3
stephenmaina2995 <stephenmaina2995@gmail.com>
2023-06-06 20:11:20 +0300

    Added a useEffect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from 'react';
+import destination from './destination';
 
 function App() {
+  const[data, setData]= useState([])
+  const [endpoint, setEndpoint]= useState([])
+
+  useEffect(() =>{
+    fetch('http://127.0.0.1:8000/destinations')
+    .then(response => response.json())
+    .then(data => console.log(data))
+  },[]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <destination/> */}
+      <p>{destination.name}</p>
+      <p>{destination.location}</p>
+      <p>{destination.image}</p>
+      <h1>Travel </h1>
     </div>
   );
 }
---
+++ src/destination.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function destination() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default destination
