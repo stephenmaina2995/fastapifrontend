324dcbb6dcc0744b1664dd43e73be67e1712dc2e
stephenmaina2995 <stephenmaina2995@gmail.com>
2023-06-07 11:44:22 +0300

    Modified Cards

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 // import logo from './logo.svg';
 import './App.css';
 import {useEffect, useState} from 'react';
-import destination from './destination';
+import Destination from './components/Destination';
+import Header from './components/Header';
 
 function App() {
   const[data, setData]= useState([])
@@ -13,13 +14,17 @@ function App() {
     .then(data => console.log(data))
   },[]);
 
+  const[list, setList] = useState([]);
+
   return (
     <div className="App">
+      <Header/>
+      <Destination/>
       {/* <destination/> */}
-      <p>{destination.name}</p>
+      {/* <p>{destination.name}</p>
       <p>{destination.location}</p>
-      <p>{destination.image}</p>
-      <h1>Travel </h1>
+      <p>{destination.image}</p> */}
+      {/* <h1>Travel </h1> */}
     </div>
   );
 }
---
+++ src/components/Destination.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from 'react';
+
+function Destination() {
+  const [data, setData] = useState([]);
+  const [endpoint, setEndpoint] = useState('http://127.0.0.1:8000/destinations');
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(endpoint);
+      const destinations = await response.json();
+      return destinations;
+    } catch (error) {
+      console.log('Error in fetching destinations: ', error);
+      return [];
+    }
+  };
+
+  useEffect(() => {
+    fetchData()
+      .then((res) => {
+        setData(res);
+      })
+      .catch((err) => {
+        console.log('Error in fetching destinations: ', err);
+      });
+  }, []);
+
+  return (
+    <div style={{width:'50%'}}>
+    <div className='destination-item flex'>
+      {/* <h1>Travel</h1> */}
+      {data.map((item) => (
+        <div key={item.id}>
+          <img src={item.image} alt={item.name} width="100%"/>
+          <p>{item.name}</p>
+          <p>{item.location}</p>
+          <p>{item.description}</p>
+          <button>Interested</button>
+          
+        </div>
+      ))}
+    </div>
+    </div>
+  );
+}
+
+export default Destination;
